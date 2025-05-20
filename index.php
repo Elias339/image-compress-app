4d5d92c02c303511aec8a8617d63fdc8c6877a00
Elias339 <eliasmd339@gmail.com>
2025-05-21 02:27:15 +0600

    input form has added in index file

--- index.php
+++ index.php
@@ -6,6 +6,10 @@
     <title>Image compresser</title>
 </head>
 <body>
-    
+    <h2>Upload an Image To Compress</h2>
+    <form action="compress.php" method="POST" enctype="multipart/form-data">
+        <input type="file" name="image" >
+        <button type="submit">Compress to WebP</button>
+    </form>
 </body>
 </html>
